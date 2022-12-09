9e4c94a55ba162bfc66aca3f1427a7fdadb11e9b
MM150551 <107167808+MM150551@users.noreply.github.com>
2022-12-09 22:51:29 +0200

    Update sketch_dec9a.ino.cpp

--- sketch_dec9a.ino.cpp
+++ sketch_dec9a.ino.cpp
@@ -1,11 +1,6 @@
-#include <Arduino.h>
-#line 1 "C:\\Users\\mina1\\AppData\\Local\\Temp\\.arduinoIDE-unsaved2022119-5264-dm1cpg.pmp4\\sketch_dec9a\\sketch_dec9a.ino"
+
 int d = 3000;
-#line 2 "C:\\Users\\mina1\\AppData\\Local\\Temp\\.arduinoIDE-unsaved2022119-5264-dm1cpg.pmp4\\sketch_dec9a\\sketch_dec9a.ino"
-void setup();
-#line 10 "C:\\Users\\mina1\\AppData\\Local\\Temp\\.arduinoIDE-unsaved2022119-5264-dm1cpg.pmp4\\sketch_dec9a\\sketch_dec9a.ino"
-void loop();
-#line 2 "C:\\Users\\mina1\\AppData\\Local\\Temp\\.arduinoIDE-unsaved2022119-5264-dm1cpg.pmp4\\sketch_dec9a\\sketch_dec9a.ino"
+
 void setup() {
   // put your setup code here, to run once:
 
